e8e4387d1905c668eaa44fb4800a7f0400a71e75
Seyed Mohammad Hossein Mazhari <smh.mazhari@gmail.com>
2024-11-28 15:03:57 +0330

    fix app mult

--- CA3/app_mult.v
+++ CA3/app_mult.v
@@ -1,11 +1,14 @@
 module app_mult(input clk ,
                 input start,
                 input rst,
-                output Done
+                input [15:0] A,
+                input [15:0] B,
+                output Done,
+                output [15:0] Result
 );
     wire shlA,loadA,shlB,loadB,rst5,cntU,cntD,shrOut,loadOut,DoneA,DoneB,downDone;
 
-    datapath DP (clk,rst,A,B,shlA,loadA,shlB,loadB,rst5,cntU,cntD,shrOut,loadOut,DoneA,DoneB,downDone);
+    datapath DP (clk,rst,A,B,shlA,loadA,shlB,loadB,rst5,cntU,cntD,shrOut,loadOut,DoneA,DoneB,downDone,Result);
     controller CU(clk,rst,start,DoneA,DoneB,downDone,Done,rst5,loadA,loadB,shlA,shlB,cntU,cntD,loadOut,shrOut);
 
 endmodule
