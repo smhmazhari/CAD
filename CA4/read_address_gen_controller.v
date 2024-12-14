58be48e5c8de18af40e9c1b53fe8494f29895225
Seyed Mohammad Hossein Mazhari <smh.mazhari@gmail.com>
2024-12-14 18:05:32 +0330

    add read_address_generator_controller

---
+++ CA4/read_address_gen_controller.v
@@ -0,0 +1,44 @@
+module read_address_gen_controller (input clk,
+                   input rst,
+                   input inner_rst,
+                   input can_count,
+                   input start,
+                   output load_registers,
+                   );
+    parameter Wait = 1'b0 , Count = 1'b1;
+    reg ps;
+    reg ns;
+
+    // sequential part
+    always @(posedge clk) begin
+        if(rst == 1'b1 || inner_rst == 1'b1) 
+            ps <= 2'b0;
+        else ps <= ns;
+    end
+
+    // combinational part (next state)
+    always @(*) begin
+        ns = Wait ;
+        case(ps)
+            Wait : ns = (start == 1'b0) ? Wait :(can_count == 1'b1)? Count : Wait;
+            Count : ns = (can_count == 1'b1) ? Count : Wait ;
+            default : ns = Wait ;
+        endcase
+    end
+
+    // combinational part (primary output)
+    always @(*) begin
+        load_registers = 1'b0;
+
+        case (ps)
+            Wait :  
+                begin
+                    load_registers = (can_count == 1'b1) ? 1'b1 : 1'b0 ;
+                end
+            Count :
+                begin
+                    load_registers = 1'b1 ;
+                end
+        endcase
+    end
+endmodule
